fc68ddef5243568f03b76aad07a10ccbe70f77b9
Yago <yago.rc.04@gmail.com>
2024-10-22 12:00:09 +0200

    Se ha exterbalizado el script a un archvio de js

---
+++ controller.js
@@ -0,0 +1,7 @@
+// Obtener el botón por su ID
+const boton = document.getElementById('add');
+
+// Añadimos un evento de clic al botón
+boton.addEventListener('click', function() {
+    alert('¡Has hecho clic en el botón!');
+});
